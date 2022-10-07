6be498df475616485dd078abb6c3fcd09e0f2b44
Timmy Lau <timmy12lau@gmail.com>
2022-10-06 20:18:46 -0700

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ResolutionAssignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
